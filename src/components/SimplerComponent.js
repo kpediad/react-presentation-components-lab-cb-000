d08db51723bc72430b6f5a5b2de16ca08652b5ab
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 11:15:01 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SimplerComponent = (props) => {
+const SimplerComponent = (props) => 
   <div onClick={props.handleClick}>I am just happy</div>
-}
+
 
 export default SimplerComponent;
